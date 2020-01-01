d97b184c3f2b502a8893ee9f6de0cfb0ca9ac100
junil-hwang <tjsdlf4261@naver.com>
2020-01-01 21:49:47 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f gh-pages
+git push -f git@github.com:JunilHwang/TIL.git gh-pages
 
 cd -
