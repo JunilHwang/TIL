308c49056e376c94037b236ee85740a0134b06b8
junil-hwang <tjsdlf4261@naver.com>
2020-01-01 21:52:17 +0900

    deploy 성공

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:JunilHwang/TIL.git gh-pages
+git push -f https://github.com/JunilHwang/TIL.git master:gh-pages
 
 cd -
