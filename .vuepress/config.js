b3c5c02f6186875b2dd98d535db0d23fc31860e5
junilhwang <tjsdlf4261@naver.com>
2020-12-24 14:20:56 +0900

    feat: feed options 추가 완료

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,4 @@
-const sidebar = require('./sidebar')
+const sidebar = require('./sidebar');
 
 module.exports = {
   title: '개발자 황준일',
@@ -6,8 +6,8 @@ module.exports = {
   themeConfig: {
     logo: 'https://avatars1.githubusercontent.com/u/18749057?s=460&v=4',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Repository', link: 'https://github.com/junilhwang/TIL/' },
+      {text: 'Home', link: '/'},
+      {text: 'Repository', link: 'https://github.com/junilhwang/TIL/'},
     ],
     sidebar,
     lastUpdated: 'Last Updated',
@@ -15,7 +15,7 @@ module.exports = {
   },
   base: '/TIL/',
   head: [
-    ['meta', { name: "google-site-verification", content: "sHfBWIoCUOYFXJ3b0ulN8jp9jpD8SEW5Wpxvlk-UABA" }],
+    ['meta', {name: "google-site-verification", content: "sHfBWIoCUOYFXJ3b0ulN8jp9jpD8SEW5Wpxvlk-UABA"}],
   ],
   markdown: {
     extendMarkdown: md => {
@@ -25,15 +25,24 @@ module.exports = {
     }
   },
   plugins: [
-    ['@vuepress/pwa', { serviceWorker: true, updatePopup: true }],
-    ['feed', { canonical_base: 'https://junilhwang.github.io/TIL/' }],
+    ['@vuepress/pwa', {serviceWorker: true, updatePopup: true}],
+    ['feed', {
+      canonical_base: 'https://junilhwang.github.io/TIL/',
+      author: {
+        name: "JunilHwang",
+        email: "junil.h@kakao.com",
+        link: "https://junilhwang.github.io/"
+      },
+      posts_directories: ["/Writing/", "/Review/", "/Book/", "/Javascript/", "/CodeSpitz/"],
+      sort: entries => entries.sort((a, b) => new Date(b.date) - new Date(a.date))
+    }],
     [
       '@vuepress/google-analytics',
       {
         'ga': 'UA-113171398-2'
       }
     ],
-    ['sitemap', { hostname: 'https://junilhwang.github.io/TIL' }],
+    ['sitemap', {hostname: 'https://junilhwang.github.io/TIL'}],
     ['demo-block']
   ]
 }
