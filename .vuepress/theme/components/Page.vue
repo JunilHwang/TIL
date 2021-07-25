f991f53927666e90e35bd0b94d850eab6475423a
JunilHwang <junil.h@kakao.com>
2021-07-25 22:05:14 +0900

    feat: 조회수 자동화

--- .vuepress/theme/components/Page.vue
+++ .vuepress/theme/components/Page.vue
@@ -23,7 +23,7 @@ import PageEdit from '@theme/components/PageEdit.vue'
 import PageNav from '@theme/components/PageNav.vue'
 import Comment from '@theme/components/Comment.vue'
 
-const host = location.origin;
+let host = '';
 
 export default {
   components: { PageEdit, PageNav, Comment },
@@ -36,6 +36,10 @@ export default {
       return `https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=${fullPath}&count_bg=%230099FF&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=%EC%A1%B0%ED%9A%8C%EC%88%98&edge_flat=true`
     }
   },
+
+  mounted () {
+    host = location.origin;
+  }
 
 }
 </script>
