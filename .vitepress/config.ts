333b66d2e33650ca10ba0555b958e701b8c25d39
JunilHwang <junil.h@kakao.com>
2022-06-02 12:49:08 +0900

    feat: config 완료

---
+++ .vitepress/config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vitepress'
+
+export default defineConfig({
+  title: '개발자 황준일',
+  description: 'Today I leanred',
+  lang: 'ko',
+  themeConfig: {
+    logo: 'https://avatars1.githubusercontent.com/u/18749057?s=460&v=4',
+  },
+  base: '/TIL/',
+})
