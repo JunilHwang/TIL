4bdaaebb89f1fde98298469c6b1615109a33626d
JunilHwang <junil.h@kakao.com>
2022-06-02 17:31:26 +0900

    feat: About 페이지 추가 완료

---
+++ .vitepress/theme/components/About.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <main>
+    <h1>About</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 30px
+}
+main {
+  display: block;
+  margin: 50px auto;
+  max-width: 800px;
+  width: 100%;
+}
+</style>
---
+++ About/index.md
@@ -0,0 +1,12 @@
+---
+
+layout: page
+hitUrl: https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://junilhwang.github.io/TIL/
+
+---
+
+<script setup>
+import About from "../.vitepress/theme/components/About.vue";
+</script>
+
+<About />
