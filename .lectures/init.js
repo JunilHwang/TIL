c42b18d8ca70dcb5d6c531f4ceaf587ecd3fe9b8
junilhwang <junil.h@kakao.com>
2022-08-27 15:31:26 +0900

    feat: reveal.js 세팅 완료

---
+++ .lectures/init.js
@@ -0,0 +1,15 @@
+import Reveal from "../node_modules/reveal.js/dist/reveal.esm.js";
+import RevealMarkdown from '../node_modules/reveal.js/plugin/markdown/markdown.esm.js';
+import RevealHighlight from '../node_modules/reveal.js/plugin/highlight/highlight.esm.js';
+
+const reveal = new Reveal({
+  width: 1800,
+  height: 1100,
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  slideNumber: true,
+  plugins: [RevealMarkdown, RevealHighlight]
+})
+reveal.initialize();
