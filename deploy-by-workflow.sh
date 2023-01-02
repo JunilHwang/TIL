267ef1b28c3dc0312b25c6d484e94876c1ab5f59
junilhwang <junil.h@kakao.com>
2023-01-02 23:00:50 +0900

    docs: 2023회고 오탈자 수정

---
+++ deploy-by-workflow.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+GITHUB_TOKEN=$GITHUB_TOKEN
+
+# abort on errors
+set -e
+
+# build
+yarn docs:build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+# gh-pages clone
+git clone -b gh-pages2 https://github.com/JunilHwang/TIL/
+cp -rf TIL/.git ./.git
+rm -rf TIL
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git add .
+git commit -m "$*"
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push https://${GITHUB_TOKEN}@github.com/JunilHwang/TIL/ gh-pages2
+
+cd -
