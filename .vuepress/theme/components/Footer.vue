6885463b9731a0110e32aaedee63150a6c4c8b8b
junilhwang <junil.h@kakao.com>
2025-06-19 12:38:13 +0900

    doc: 자기소개 내용 추가

--- .vuepress/theme/components/Footer.vue
+++ .vuepress/theme/components/Footer.vue
@@ -2,13 +2,12 @@
   <footer class="siteFooter">
     <div class="container">
       <p class="siteFooterCopyright">
-        Copyright &copy; 2019 All Right <em>개발자 황준일</em>
+        Copyright &copy; {{ new Date().getFullYear() }} All Right <em>개발자 황준일</em>
       </p>
       <p class="siteFooterContact">
         junil.h@kakao.com .
         +82-10-5764-4483 .
         <a href="https://github.com/junilhwang" target="_blank">GitHub</a> .
-        <a href="http://junil-hwang.com" target="_blank">Blog</a> .
         <a href="https://github.com/junilhwang/TIL/" target="_blank">Repository</a>
       </p>
     </div>
